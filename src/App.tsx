f78436fe0883890b45bf5a31af984cf034809e7b
Rafael Puchades <rpuchadm@cfp.upv.es>
2024-12-16 20:09:37 +0100

    bootswatch pulse

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import "bootswatch/dist/darkly/bootstrap.min.css"
 // import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+import Layout from "./components/layout/Layout.tsx"
 import HomePage from "./components/HomePage"
 import AboutPage from "./components/AboutPage"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <Layout>
+      <br />
+      <Router>
+        <Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </Layout>
   )
 }
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import "bootswatch/dist/materia/bootstrap.css"
+import "bootswatch/dist/pulse/bootstrap.min.css"
 
 import Col from "react-bootstrap/Col"
 import Container from "react-bootstrap/Container"
